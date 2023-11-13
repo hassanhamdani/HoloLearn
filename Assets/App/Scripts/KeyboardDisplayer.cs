1c9ce28ae564b765b677801151dd289287b22296
hassanhamdani <96621474+hassanhamdani@users.noreply.github.com>
2023-11-13 23:48:52 +0400

    Added Page Functionality

---
+++ Assets/App/Scripts/KeyboardDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardDisplayer : MonoBehaviour
+{
+
+    public TouchScreenKeyboard keyboard;
+    public void OpenSystemKeyboard()
+    {
+    keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+}
+    // Update is called once per frame
+    void Update()
+    {
+      if (keyboard != null)
+    {
+        string keyboardText = keyboard.text;
+        // Do stuff with keyboardText
+    }  
+    }
+}
---
+++ Assets/ViewToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewToggle : MonoBehaviour
+{
+    //Serial ize field for gameobject
+    [SerializeField] GameObject viewObject;
+
+    //New field for a text mesh 
+    [SerializeField] TextMesh textMesh;
+
+    //on button click, disable or enable the gameobject
+    public void OnButtonClick()
+    {
+        //Toggle text between Off and On
+        if (textMesh.text == "Off")
+        {
+            textMesh.text = "On";
+        }
+        else
+        {
+            textMesh.text = "Off";
+        }
+
+        viewObject.SetActive(!viewObject.activeSelf);
+    }
+}
